887c105cae0f4bf81b9abea437951a4c684093e4
Dersa <dersa85@googlemail.com>
2021-06-14 10:10:39 +0200

    data-protection and imprint added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SlideshowComponent },
+  { path: 'data-protection', component: DataProtectionComponent },
+  { path: 'imprint', component: ImprintComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
  
+  currentRoute = '';
 
-  ngOnInit() {
-    
-  }
+  constructor(public router: Router) {}
 
   
 }
